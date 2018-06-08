dd88a46551bf4e786dc174f097803200948afa17
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2018-06-08 18:21:03 +0200

    add project

---
+++ js/quotes.js
@@ -0,0 +1,45 @@
+//list of quotes with their values
+var quotes = [
+    {
+        quote: "Democracy substitutes election by the incompetent many for appointment by the corrupt few.",
+        source: "George Bernard Shaw",
+        citation: "Man and Superman - \"Maxims for Revolutionists\"",
+        year: 1903,
+        tags: ['Democracy', 'life']
+    },
+    {
+        quote: "Abundance is, in large part, an attitude.",
+        source: "Sue Patton Thoele",
+        tags: ['Democracy', 'life']
+    },
+    {
+        quote: "Love is, above all else, the gift of oneself.",
+        source: "Jean Anouilh",
+        year: 1910,
+        tags: ['Love', 'life']
+    },
+    {
+        quote: "Human beings are perhaps never more frightening than when they are convinced beyond doubt that they are right.",
+        source: "Laurens Van der Post",
+        citation: "The Lost World of the Kalahari",
+        year: 1958,
+        tags: ['Human', 'life']
+    },
+    {
+        quote: "Let no one ever come to you without leaving better and happier.",
+        source: "Mother Teresa",
+        year: 1997,
+        tags: ['Happiness', 'life']
+    },
+    {
+        quote: "Fear sometimes stops you from doing stupid things.",
+        source: "Helen Mirren",
+        tags: ['Fear', 'life']
+    },
+    {
+        quote: "Success is relative. It is what we can make of the mess we have made of things.",
+        source: "T. S. Eliot",
+        year: 1888,
+        tags: ['Success', 'life']
+    }
+];
---
+++ js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+// get a random number between 0 and the length of the quotes
+// return a random quote
+const getRandomQuote = () => {
+    let max = quotes.length;
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    let randomNum = getRandomInt(max);
+    return quotes[randomNum];
+};
+
+const randomColor = () => {
+    let color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    return color;
+};
+
+// new quote is generated
+// create the html with the new quote data
+// change the html of the page by modifying the innerHtml of the quote-box
+const printQuote = () => {
+    const newQuote = getRandomQuote();
+    let quoteHtml = `<p class="quote">${newQuote.quote}</p>`;
+    quoteHtml += `<p class="source">${newQuote.source}`;
+    if(newQuote.citation) {
+        quoteHtml += `<span class="citation">${newQuote.citation}</span>`;
+    }
+    if(newQuote.year){
+        quoteHtml += `<span class="citation">${newQuote.year}</span>`;
+    }
+    if(newQuote.tags){
+        let tags = newQuote.tags;
+        tags.forEach(function(tag) {
+            quoteHtml += `<p class="tags">${tag}</p>`;
+        });
+
+    }
+    quoteHtml += `</p>`;
+    document.getElementById('quote-box').innerHTML = quoteHtml;
+    document.body.style.backgroundColor = randomColor();
+
+
+};
+let printQuoteClearInterval = () => {
+    printQuote();
+    clearInterval(printQuote);
+};
+
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuoteClearInterval, false);
+clearInterval(printQuote);
+setInterval(printQuote, 3000);
+
+
